a3542b96ca1ba80c8d78ca7d1e6b715526bcdd0a
Sean <seanpoulter@hotmail.com>
2022-08-26 14:42:27 -0400

    Remove js for creating popup and now create popup in template.html

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './style.css';
 import { createTodoList } from './createTodo.js';
 import { createPopup } from './createPopup.js';
 
-
 // add event listener to know which function to run
 // sidebar.addEventListener('click', (event) => {
 createPopup(info)
